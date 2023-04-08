33a7f6ab25d1b8324228be661c094dbfe600f48e
gracefour11 <gracecheam@gmail.com>
2023-04-08 23:23:31 +0800

    remove tutorial

--- server.js
+++ server.js
@@ -76,9 +76,6 @@ app.use(express.static(path.join(__dirname, "public")));
  */
 require('./app/routes/passport.routes.js')(app, passport);
 
-const tutorial = require("./app/routes/tutorial.routes.js");
-app.use("/tutorial", tutorial);
-
 const user = require("./app/routes/user.routes.js");
 app.use("/home/profile", user);
 
