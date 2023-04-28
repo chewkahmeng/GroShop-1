3a67c7c4551366dee87e801e7ed92c97d37d47cc
gracefour11 <gracecheam@gmail.com>
2023-04-28 22:05:12 +0800

    update insert_user data script

--- db scripts/insert_user_data.sql
+++ db scripts/insert_user_data.sql
@@ -1,36 +1,43 @@
 use userservice;
+
+----------------------------------
+-- TRUNCATING TABLES
+----------------------------------
+truncate userservice.tbl_user;
+truncate userservice.tbl_address;
+
 ----------------------------------
 -- INSERTING ADMIN USERS
 ----------------------------------
 -- Employee1@example.com/Employee1
 insert into userservice.tbl_user
-(username, email, password, role)
-values("Employee1", "Employee1@example.com", "$2b$10$t5AD.ZWwNEBV5G3WwyWv7.QQi.LT60P/3gTVnAiSqAjD4aodGEBga" , "ADMIN");
+(id, username, email, password, role)
+values(1, "Employee1", "Employee1@example.com", "$2b$10$t5AD.ZWwNEBV5G3WwyWv7.QQi.LT60P/3gTVnAiSqAjD4aodGEBga" , "ADMIN");
 
 -- Employee2@example.com/Employee2
 insert into userservice.tbl_user
-(username, email, password, role)
-values("Employee2", "Employee2@example.com", "$2b$10$oc4eqeYchkUfnQnCcPEO6eHRt8AiJDbqUya.krIowFV9tqxW7vN8C" , "ADMIN");
+(id, username, email, password, role)
+values(2, "Employee2", "Employee2@example.com", "$2b$10$oc4eqeYchkUfnQnCcPEO6eHRt8AiJDbqUya.krIowFV9tqxW7vN8C" , "ADMIN");
 
 -- Employee3@example.com/Employee3
 insert into userservice.tbl_user
-(username, email, password, role)
-values("Employee3", "Employee3@example.com", "$2b$10$AfuBqWfKoQOZ9YUhHO1KPOq9RlvLDCjMDliVtkfZBInt5IJXavEDi" , "ADMIN");
+(id, username, email, password, role)
+values(3, "Employee3", "Employee3@example.com", "$2b$10$AfuBqWfKoQOZ9YUhHO1KPOq9RlvLDCjMDliVtkfZBInt5IJXavEDi" , "ADMIN");
 
 ----------------------------------
 -- INSERTING CUSTOMER USERS
 ----------------------------------
 -- Customer1@example.com/Customer1
 insert into userservice.tbl_user
-(username, email, password, role)
-values("Customer1", "Customer1@example.com", "$2b$10$t5AD.ZWwNEBV5G3WwyWv7.QQi.LT60P/3gTVnAiSqAjD4aodGEBga" , "CUSTOMER");
+(id, username, email, password, role)
+values(4, "Customer1", "Customer1@example.com", "$2b$10$TdIBuYD9L39MJkPvoMu6NOKvf4pTZlHk8qOz.viRrE2wyHFQRHW1S" , "CUSTOMER");
 
 -- Customer2@example.com/Customer2
 insert into userservice.tbl_user
-(username, email, password, role)
-values("Customer2", "Customer2@example.com", "$2b$10$KfvLLc47gUimFo1xfI3IDuQrgQmLR1MfNOao9GquiMjh1OXiBTPaC" , "CUSTOMER");
+(id, username, email, password, role)
+values(5, "Customer2", "Customer2@example.com", "$2b$10$KfvLLc47gUimFo1xfI3IDuQrgQmLR1MfNOao9GquiMjh1OXiBTPaC" , "CUSTOMER");
 
 -- Customer3@example.com/Customer3
 insert into userservice.tbl_user
-(username, email, password, role)
-values("Customer3", "Customer3@example.com", "$2b$10$KRHgQx2JGD7mUp/.2r.FzOcXMkDz3qExxRTeB0Vi30XWjIgXIsJ4a" , "CUSTOMER");
+(id, username, email, password, role)
+values(6, "Customer3", "Customer3@example.com", "$2b$10$KRHgQx2JGD7mUp/.2r.FzOcXMkDz3qExxRTeB0Vi30XWjIgXIsJ4a" , "CUSTOMER");
