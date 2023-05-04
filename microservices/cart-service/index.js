e3783d201ef178a7278634b7efeb28b09944299a
khairulanwarDeen <khairulanwarbinsharifuddeen@gmail.com>
2023-05-04 14:19:30 +0800

    cart service update, create, insert, delete from cart

--- microservices/cart-service/index.js
+++ microservices/cart-service/index.js
@@ -4,12 +4,44 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
+const uuid = require('uuid');
 //const db = require('./db.js');
 
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+// "Promise" wrapped database class
+class Database {
+  constructor( config ) {
+      this.connection = mysql.createConnection( {
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        multipleStatements : true
+      } );
+  }
+  query( sql, args ) {
+      return new Promise( ( resolve, reject ) => {
+          this.connection.query( sql, args, ( err, rows ) => {
+              if ( err )
+                  return reject( err );
+              resolve( rows );
+          } );
+      } );
+  }
+  close() {
+      return new Promise( ( resolve, reject ) => {
+          this.connection.end( err => {
+              if ( err )
+                  return reject( err );
+              resolve();
+          } );
+      } );
+  }
+}
 
 const db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
@@ -26,11 +58,11 @@ db.connect(function(err) {
 })
 
 // microservice api to be called in cart.routes.js
-app.get("/:id/api", (req, res) => {
+app.get("/:id/getcart", (req, res) => {
   const userid =req.params.id;
   const q = `
   SELECT 
-	  productId
+	  a.id, a.name, a.amount, a.uom, b.cartId
 	FROM 
 		cartservice.productincart as a 
 	inner join 
@@ -51,6 +83,116 @@ app.get("/:id/api", (req, res) => {
 
   })
 })
+
+app.get("/:id/addtocart", (req, res) => {
+  const database=  new Database();
+  const userid =req.params.id;
+  const ingredients = req.body.ingredients;
+  const newCartId = uuid.v1();
+  console.log(JSON.stringify(ingredients))
+  database.query( 
+    `SELECT * FROM cartservice.cart where cartStatus = "pending" and userID ="${userid}";` )
+    .then( rows => {
+        console.log("rows", JSON.stringify(rows))
+        
+        if(JSON.stringify(rows) == "[]"){
+          console.log("create new cart here")
+          
+          console.log("UUID HERE ", newCartId)
+          database.query( 
+            `
+            INSERT INTO cartservice.cart (userId, cartId, cartStatus) VALUES ('${userid}', '${newCartId}', 'pending');`
+            )
+          for(var i = 0; i < ingredients.length; i++){
+            if(ingredients[i+1]== undefined){
+              console.log("last element of the array");
+              console.log("inserting the following ", ingredients[i].name,ingredients[i].amount,ingredients[i].uom,)
+              return database.query( 
+                `INSERT INTO cartservice.productincart (name, cartId, amount, uom) VALUES ('${ingredients[i].name}', '${newCartId}' , '${ingredients[i].amount}', '${ingredients[i].uom}');`
+                )
+            }else{
+              database.query( 
+                `INSERT INTO cartservice.productincart (name, cartId, amount, uom) VALUES ('${ingredients[i].name}', '${newCartId}' , '${ingredients[i].amount}', '${ingredients[i].uom}');`
+                )
+                console.log("inserting the following ", ingredients[i].name,ingredients[i].amount,ingredients[i].uom,)
+            }
+            }
+        }else{
+          console.log("JSON.stringify(rows[0].cartId): ", JSON.stringify(rows[0].cartId))
+          const existingCartId = JSON.stringify(rows[0].cartId);
+          console.log("existingCartId ", existingCartId)
+          console.log("inserting ingredients to old cart here")
+          for(var i = 0; i < ingredients.length; i++){
+            if(ingredients[i+1]== undefined){
+              console.log("last element of the array");
+              console.log("inserting the following ", ingredients[i].name,ingredients[i].amount,ingredients[i].uom,);
+              return database.query( 
+                `INSERT INTO cartservice.productincart (name, cartId, amount, uom) VALUES ('${ingredients[i].name}', ${existingCartId} , '${ingredients[i].amount}', '${ingredients[i].uom}');`
+                )
+                
+            }else{
+              database.query( 
+                `INSERT INTO cartservice.productincart (name, cartId, amount, uom) VALUES ('${ingredients[i].name}', ${existingCartId} , '${ingredients[i].amount}', '${ingredients[i].uom}');`
+                )
+                console.log("inserting the following ", ingredients[i].name,ingredients[i].amount,ingredients[i].uom,)
+
+            }
+
+          }
+        }
+    }, err =>{
+      return database.close().then( () => { throw err; } )
+    } ).then(output =>{
+      if(output == undefined){
+        return res.status(400).send({
+          error: "Something went wrong here!",
+          output: output
+        });
+      }else{
+        return res.status(200).send({
+          message: "Ingredients Added to Cart!",
+          output: output
+        });
+      }
+
+      database.close()
+      
+    }).catch( err =>{
+      return res.json(err);
+    })
+  // const q = `
+  // INSERT INTO cartservice.productincart (productId, cartId, quantity) VALUES ('4', '1', '4');`
+  // for(let i = 0; i<4;i++){
+  //     db.query(q, (err, data)=> {
+  //   if(err){
+  //     return res.json(err)
+  //   }
+  //   else{
+  //     return res.json(data)
+  //   }
+
+  // })
+  // }
+
+})
+
+app.get("/:id/removefromcart", (req, res) => {  
+  const userid =req.params.id;
+  const itemId =req.body.itemId;
+  const q = `
+  DELETE FROM cartservice.productincart WHERE (id = '${itemId}');`
+  db.query(q, (err, data)=> {
+    if(err){
+      return res.json(err)
+    }
+    else{
+      return res.json(data)
+    }
+
+  })
+})
+
+
 
 app.listen(4000, () => {
   console.log("Listening on 4000");
